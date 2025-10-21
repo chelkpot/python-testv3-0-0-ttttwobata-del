3613993f59ab7fbcfe468be0c78984e0c60edc94
ttttwobata-del <ttttwobata@gmail.com>
2025-10-22 00:00:20 +0500

    Update task5.py

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,7 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
-
+    n = int(input())
+    hours = n // 3600 % 24
+    minutes = n // 60 % 60
+    seconds = n % 60
+    print(hours, ":", str(minutes).zfill(2), ":", str(seconds).zfill(2), sep="")
 
    
 # Код ниже не трогать! он нужен для тестов
