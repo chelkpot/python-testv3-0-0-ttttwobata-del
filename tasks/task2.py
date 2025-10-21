f6da9a3e83f5c6baa1333f2496bf9b9e28dbb4a7
ttttwobata-del <ttttwobata@gmail.com>
2025-10-21 23:58:55 +0500

    Update task2.py

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,10 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-
-   
-
-   
+    n = int(input())
+    hours = (n // 60) % 24
+    minutes = n % 60
+    print(hours, minutes)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
