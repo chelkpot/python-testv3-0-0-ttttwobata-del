6ad384580703b79d75706599a8a54c25d9ee94db
ttttwobata-del <ttttwobata@gmail.com>
2025-10-21 23:59:38 +0500

    Update task3.py

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,7 +2,18 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    n = int(input())
+    count = 0
+    count += n // 100
+    n %= 100
+    count += n // 20
+    n %= 20
+    count += n // 10
+    n %= 10
+    count += n // 5
+    n %= 5
+    count += n
+    print(count)    
 
 
 # Код ниже не трогать! он нужен для тестов
